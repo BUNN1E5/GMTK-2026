d2aa42ef4431a80babe91fa02789ab020c2e4bba
BUNN1E5 <bunn1e5@users.noreply.github.com>
2026-07-31 02:19:54 -0700

    UUID issues oops

--- networking/test_connect.gd
+++ networking/test_connect.gd
@@ -18,5 +18,5 @@ func _on_create_lobby_button_pressed() -> void:
 	var uuid = UUID.v4()
 	await PeerConnect.create_lobby(PeerConnect.local_user_id)
 	await get_tree().create_timer(1.0).timeout
-	local_user_id_display.text = uuid
+	local_user_id_display.text = PeerConnect.local_user_id
 	pass
